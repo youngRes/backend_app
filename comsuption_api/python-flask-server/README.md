9a5d388a81f3ca7b7a5160fde7c111326d8de4c8
Angel <ponxosio@gmail.com>
2020-01-24 15:02:17 +0100

    added mongo connection package -- Update README.md, __init__.py, and 6 more files...

--- comsuption_api/python-flask-server/README.md
+++ comsuption_api/python-flask-server/README.md
@@ -21,13 +21,13 @@ python3 -m swagger_server
 and open your browser to here:
 
 ```
-http://localhost:8080/data_comsuption_api/ui/
+http://localhost:8080/data/ui/
 ```
 
 Your Swagger definition lives here:
 
 ```
-http://localhost:8080/data_comsuption_api/swagger.json
+http://localhost:8080/data/swagger.json
 ```
 
 To launch the integration tests, use tox:
---
+++ comsuption_api/python-flask-server/swagger_server/mongo_connection/mongo_queries.py
@@ -0,0 +1,14 @@
+"""
+This file contains all the queries to the database, it must use the
+MongoDBConnection to connect to the MOngoDB so its transparent to the user
+"""
+
+from sources.mongo_connection.mongo_connector import MongoDBConnection
+
+
+def check_connection():
+    """
+    This function prints the infor server so you can check that the connection
+    is working
+    """
+    return MongoDBConnection.driver.server_info()
