7c13f703382d655123931ebd3b60547d09f9c2e8
Angel <ponxosio@gmail.com>
2020-01-24 15:06:23 +0100

    show server info at boot --- Update __main__.py and __main__.py

--- comsuption_api/python-flask-server/swagger_server/__main__.py
+++ comsuption_api/python-flask-server/swagger_server/__main__.py
@@ -2,10 +2,17 @@
 
 import connexion
 
+import swagger_server.mongo_connection.mongo_queries as dbq
 from swagger_server import encoder
+from swagger_server.mongo_connection import MongoDBConnection
 
 
 def main():
+    #initilize connection with MongoDB
+    MongoDBConnection.initialize_connection('db', 270017)
+    print(dbq.check_connection())
+
+    # start the api server
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'YoungRes data comsuption API'}, pythonic_params=True)
--- gameplay_api/python-flask-server/swagger_server/__main__.py
+++ gameplay_api/python-flask-server/swagger_server/__main__.py
@@ -2,10 +2,18 @@
 
 import connexion
 
+import swagger_server.mongo_connection.mongo_queries as dbq
 from swagger_server import encoder
+from swagger_server.mongo_connection.mongo_connector import MongoDBConnection
+
 
 
 def main():
+    #initilize connection with MongoDB
+    MongoDBConnection.initialize_connection('db', 270017)
+    print(dbq.check_connection())
+
+    # start the api server
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'YoungRes gameplay API'}, pythonic_params=True)
