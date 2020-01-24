283af4999686f1cf7189fe13e64fe6933e49ab92
Angel <ponxosio@gmail.com>
2020-01-24 15:41:15 +0100

    hotfix -- Update __main__.py

--- comsuption_api/python-flask-server/swagger_server/__main__.py
+++ comsuption_api/python-flask-server/swagger_server/__main__.py
@@ -4,13 +4,12 @@ import connexion
 
 import swagger_server.mongo_connection.mongo_queries as dbq
 from swagger_server import encoder
-from swagger_server.mongo_connection import MongoDBConnection
+from swagger_server.mongo_connection.mongo_connector import MongoDBConnection
 
 
 def main():
     #initilize connection with MongoDB
     MongoDBConnection.initialize_connection('db', 270017)
-    print(dbq.check_connection())
 
     # start the api server
     app = connexion.App(__name__, specification_dir='./swagger/')
