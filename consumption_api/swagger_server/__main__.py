3c606689f4f04f08b7d750f88da8d41686312480
angel <angel@angel-UX410UAR>
2020-10-15 10:59:38 +0200

    Allow CORS on server by default -- Actualizar requirements.txt y __main__.py

--- consumption_api/swagger_server/__main__.py
+++ consumption_api/swagger_server/__main__.py
@@ -2,6 +2,7 @@
 
 import connexion
 
+from flask_cors import CORS
 from swagger_server import encoder
 from swagger_server.mongo_connection.mongo_connector import MongoDBConnection
 
@@ -10,6 +11,7 @@ def main():
     MongoDBConnection.initialize_connection('db', 27017)
 
     app = connexion.App(__name__, specification_dir='./swagger/')
+    CORS(app.app)
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'YoungRes data comsuption API'}, pythonic_params=True)
     app.run(port=8080)
